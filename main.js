a45c43b45a775dfa7efe3abd996cf2e9e43d60f7
Tommhsieh <mark29737148mark@gmail.com>
2025-04-01 22:51:24 +0800

    update_tom

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let countdown;
 let timeLeft = 0;
 
+//開始
 function startTimer() {
     let inputSeconds = document.getElementById("seconds").value;
+    //防止輸入負數或0、非數字
     if (isNaN(inputSeconds) || inputSeconds <= 0) {
         alert("請輸入有效的秒數！");
         return;
@@ -15,7 +17,7 @@ function startTimer() {
     countdown = setInterval(() => {
         timeLeft--;
         updateDisplay(timeLeft);
-
+ㄋ
         if (timeLeft <= 0) {
             clearInterval(countdown);
             document.getElementById("timer").textContent = "時間到！";
@@ -23,19 +25,23 @@ function startTimer() {
     }, 1000);
 }
 
+//暫停
 function stopTimer() {
     clearInterval(countdown);
 }
 
+//重設
 function resetTimer() {
     clearInterval(countdown);
     document.getElementById("timer").textContent = "00:00";
     document.getElementById("seconds").value = 10;
 }
 
+//修改顯示內容
 function updateDisplay(seconds) {
     let min = Math.floor(seconds / 60);
     let sec = seconds % 60;
+    //.padStart(2, '0') 是 JavaScript 的字串方法，用來補齊字串長度。如果字串長度小於 2，則在前面補 0，直到長度達到 2。
     document.getElementById("timer").textContent =
         String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0');
 }
